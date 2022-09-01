e05ac818bbafc5bb5ece66fd8ab1d598cc117be7
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-09-01 23:32:42 +0530

    import java.util.Scanner; class multiplication {  public static void main(String args[])     {      Scanner sc=new Scanner(System.in);      int a,b,c;      a=sc.nextInt();      b=sc.nextInt();      c=a*b;
         System.out.print(c);
        }
    }

---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class multiplication
+{
+ public static void main(String args[])
+    { 
+     Scanner sc=new Scanner(System.in);
+     int a,b,c;
+     a=sc.nextInt();
+     b=sc.nextInt();
+     c=a*b;
+    
+     System.out.print(c);
+    }
+}
+     
+     
