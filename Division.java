19cdfbdc23f7ea64f41eb57be12c5b92bbeb0d1f
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-09-01 12:17:17 +0530

    import java.util.Scanner; class Triangle {  public static void main(String args[])     {      Scanner sc=new Scanner(System.in);         int a,b,c;         float s,area;         a=sc.nextInt();         b=sc.nextInt();         c=(int)(a/b);         System.out.format("%d",c);  } }

---
+++ Division.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Triangle
+{
+ public static void main(String args[])
+    { 
+     Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        float s,area;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=(int)(a/b);
+        System.out.format("%d",c);
+ }
+}
