a561a57845a0f0c6d816ee9e55b257e0f8a96ba4
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-09-10 13:21:02 +0530

    import java.util.Scanner; class Take_input {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         System.out.print(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Take_input
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.print(s);
+    }
+}
