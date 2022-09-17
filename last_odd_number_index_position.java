410b4f51d2fd260d210dd8f0d2754b800d985297
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-09-17 15:28:04 +0530

    import java.util.Scanner; class odd {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],n,i;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(i=n-1;i>=0;i--)         {             if(x[i]%2!=0)             {                 System.out.print(i);                 break;             }         }     } }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class odd
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x[],n,i;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(i=n-1;i>=0;i--)
+        {
+            if(x[i]%2!=0)
+            {
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+}
