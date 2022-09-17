d1d9400c94b217f79b6c65af717330e6187e1c60
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-09-17 10:45:02 +0530

    import java.util.Scanner; class Primes_greater_than_k {     public static boolean isprime(int n)     {         if (n<2)         {             return false;         }         for(int i=2;i<n;i++)         {             if (n%i==0)             {                 return false;             }         }         return true;     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,k;         int c=0;         n=sc.nextInt();         int[] x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(int ele:x)         {             if(isprime(ele))             {                   c++;             }         }         System.out.print(c);     }
    }

---
+++ Primes_in_the_array.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class Primes_greater_than_k
+{
+    public static boolean isprime(int n)
+    {
+        if (n<2)
+        {
+            return false;
+        }
+        for(int i=2;i<n;i++)
+        {
+            if (n%i==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,k;
+        int c=0;
+        n=sc.nextInt();
+        int[] x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(int ele:x)
+        {
+            if(isprime(ele))
+            {
+                  c++;
+            }
+        }
+        System.out.print(c);
+    }
+    
+}
