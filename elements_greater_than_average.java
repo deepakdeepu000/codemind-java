e4ec978f2b04ffb51eb795ec642eb1f74f0309fc
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-09-22 13:43:09 +0530

    import java.util.Scanner; class First { 	public static void main(String args[]) 	{ 	    Scanner sc=new Scanner(System.in); 	    int a=sc.nextInt(); 	    int[] arr=new int[a]; 	    int i,k=0,l=0; 	    for(i=0;i<a;i++) 	    { 	        arr[i]=sc.nextInt(); 	        k+=arr[i]; 	    } 	    int avg; 	    avg=k/a; 	    for(int ele:arr) 	    { 	        if (ele>=avg) 	        { 	            l++; 	        } 	    } 	    System.out.print(l); 	} }

---
+++ elements_greater_than_average.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class First
+{
+	public static void main(String args[])
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    int[] arr=new int[a];
+	    int i,k=0,l=0;
+	    for(i=0;i<a;i++)
+	    {
+	        arr[i]=sc.nextInt();
+	        k+=arr[i];
+	    }
+	    int avg;
+	    avg=k/a;
+	    for(int ele:arr)
+	    {
+	        if (ele>=avg)
+	        {
+	            l++;
+	        }
+	    }
+	    System.out.print(l);
+	}
+}
