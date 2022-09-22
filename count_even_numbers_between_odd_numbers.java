5a64557380291f825652f4fa689cef36b77086c8
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-09-22 10:36:39 +0530

    import java.util.*; class Even_between_odd {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n = sc.nextInt();         int[] x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         int c=0;         for(int i=1;i<n-1;i++)         {             if(x[i-1]%2!=0 && x[i+1]%2!=0)             {                 if(x[i]%2==0)                 {                     c++;                 }             }
            }
            System.out.println(c);
        }
    }

---
+++ count_even_numbers_between_odd_numbers.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Even_between_odd
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        int c=0;
+        for(int i=1;i<n-1;i++)
+        {
+            if(x[i-1]%2!=0 && x[i+1]%2!=0)
+            {
+                if(x[i]%2==0)
+                {
+                    c++;
+                }
+            }
+            
+        }
+        System.out.println(c);
+    }
+}
