c686f0e2db9e3835f37e09753ce633976fe50033
deepakdeepu000 <99535087+deepakdeepu000@users.noreply.github.com>
2022-09-23 14:41:21 +0530

    import java.util.*; class min_bt_a_and_b {     public static void main(String args[])     {         Scanner sc =new Scanner(System.in);         int n=sc.nextInt();         int[] x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         int a=sc.nextInt();         int b=sc.nextInt();         int[] y=new int[100];         int s=0,max;         for(int i=0;i<n;i++)         {             if(x[i]>=a && x[i]<=b)             {                y[s++]=x[i];             }         }         if(s==0)         {             System.out.print(-1);
            }
            else
            {
                for(int j=0;j<s;j++)
            {
               System.out.print(y[j]+" ");     
            }
            }
    
            
            
            
        }
    }

---
+++ elements_in_between_A_and_B.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class min_bt_a_and_b
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int[] y=new int[100];
+        int s=0,max;
+        for(int i=0;i<n;i++)
+        {
+            if(x[i]>=a && x[i]<=b)
+            {
+               y[s++]=x[i]; 
+            }
+        }
+        if(s==0)
+        {
+            System.out.print(-1);
+            
+        }
+        else
+        {
+            for(int j=0;j<s;j++)
+        {
+           System.out.print(y[j]+" ");     
+        }
+        }
+
+        
+        
+        
+    }
+}
